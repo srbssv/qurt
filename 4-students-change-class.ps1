e953c9e5ae4ae0153335463cd778f368f10ca220
srbssv <isarbassov@gmail.com>
2019-01-28 11:02:07 +0600

    TEMPORARY UPDATE: add a student to a new class group, when changing his class

--- 4-students-change-class.ps1
+++ 4-students-change-class.ps1
@@ -11,7 +11,18 @@ foreach ($user in $users) {
         $LastGrade = $user.LastGrade
         $NewGrade = $user.NewGrade
         $NewOU = Get-ADOrganizationalUnit -Filter {Name -like $NewGrade} -SearchBase "OU=Students, OU=Users, OU=ALM-FM, dc=nis, dc=edu, dc=kz" -SearchScope OneLevel
-        $u = Get-ADUser -Filter {SamAccountName -like $SamAccountName} -SearchBase "OU=Students, OU=Users, OU=ALM-FM, dc=nis, dc=edu, dc=kz"
+        $u = Get-ADUser -Filter {SamAccountName -like $SamAccountName} -SearchBase "OU=Students, OU=Users, OU=ALM-FM, dc=nis, dc=edu, dc=kz" -Properties MemberOf
+        $MemberOf = $u.MemberOf
+        $LastGroupTxt = $user.LastGrade + "*"
+        $NewGroupTxt = $user.NewGrade + "*"
+        $NewGroup = Get-ADGroup -Filter {Name -like $NewGroupTxt} -SearchBase "OU=Groups, OU=ALM-FM, dc=nis, dc=edu, dc=kz"
+        foreach ($Member in $MemberOf) {
+            $GroupToRemove = Get-ADGroup $Member
+            if ($GroupToRemove.Name -like $LastGroupTxt){
+                Remove-ADGroupMember $GroupToRemove -Members $u -Confirm:$False
+            }
+        }
+        Add-ADGroupMember $NewGroup -Members $u
         Set-ADUser $u -Description $NewOU.Name
         Move-ADObject $u -TargetPath $NewOU.DistinguishedName
         $u.Name, $LastGrade, " -> ", $NewOU.Name
